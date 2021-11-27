5e2cc4298db67fb54397f85f37b93d4757ba9c96
rayves <vo.raymond.v@outlook.com>
2021-11-27 13:02:53 +1100

    wrap whole program in loop so program does not end unless explicitly inputed

---
+++ bank_app.rb
@@ -0,0 +1,46 @@
+# Write a program to implement a simple bank account. 
+
+
+
+def banking_app
+	# Your code here.
+	acc_balance = 0.to_f
+	puts "Welcome to the banking app"
+	puts "Please enter your password."
+	gets.chomp
+	puts "What would you like to do? (options: balance, deposit, withdraw or exit)"
+	while options = gets.chomp
+		case options
+		when "balance"
+			puts "Your balance is $#{acc_balance}"
+			puts "What would you like to do? (options: balance, deposit, withdraw or exit)"
+		when "deposit"
+			puts "How much would you like to deposit?"
+			acc_balance += gets.chomp.to_f
+			puts "Your balance is now $#{acc_balance}"
+			puts "Would you like to make another deposit?"
+			while deposit_loop = gets.chomp
+				system "clear"
+				case deposit_loop
+				when "yes"
+					puts "How much would you like to deposit?"
+					acc_balance += gets.chomp.to_f
+					puts "Your balance is now $#{acc_balance}"
+					puts "Would you like to make another deposit?"
+				when "no"
+					puts "What would you like to do? (options: balance, deposit, withdraw or exit)"
+					break
+				else
+					puts "Invalid selection!"
+					puts "Would you like to make another deposit?"
+				end
+			end
+		when "exit"
+			puts "Thank you for using bank app."
+			break
+		else puts "Invalid selection!"
+		end
+	end
+end
+
+banking_app
